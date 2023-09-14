f049e636100c0e0fa4e9aeb50a4e67973d236e6a
Bailey Bell Loffredo <baileybell.loffredo@gmail.com>
2023-09-14 13:57:44 -0400

    python

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list=[]
+    if length == 0:
+        print(fibonacci_list)
+        return fibonacci_list
+    if length == 1:
+        fibonacci_list.append(0)
+        print(fibonacci_list)
+        return fibonacci_list
+    current=1
+    previous=0
+    fibonacci_list.append(previous)
+    fibonacci_list.append(current)
+    while len(fibonacci_list) < length:
+        next= current + previous
+        fibonacci_list.append(next)
+        temp= current
+        current= next
+        previous= temp
+    print(fibonacci_list)
+print_fibonacci(10)
+
+
+
+    
